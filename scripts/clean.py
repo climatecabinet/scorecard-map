d8152412bbe79a97a99daa8bba6421e313f08858
shelbygreen <greenshelby@gmail.com>
2021-07-19 08:49:12 -0400

    reprojected files & removed bad ones

--- scripts/clean.py
+++ scripts/clean.py
@@ -25,41 +25,50 @@ if __name__ == "__main__":
     print("Clip GeoJSON to shoreline")
     filenames = []
     # for house files
-    for filename in sorted(glob.glob("data/geospatial/all/*House.geojson")): 
-        newfilename = filename.replace("/all/", "/state-leg/house/")
-        filenames.append(newfilename)
-        if os.path.exists(newfilename):
-            print(f"{newfilename} exists, skipping")
+    for filename in sorted(glob.glob("data/geospatial/all/*House.geojson")):
+        if filename in ['data/geospatial/all/DE-House.geojson', 'data/geospatial/all/LA-House.geojson', 'data/geospatial/all/MS-House.geojson',
+        'data/geospatial/all/NC-House.geojson', 'data/geospatial/all/OH-House.geojson']:
+            print('{filename} is a bad file. Passing.') 
         else:
-            print(f"{filename} => {newfilename}")
-            subprocess.run(
-                [
-                    "ogr2ogr",
-                    "-clipsrc",
-                    "data/geospatial/source/cb_2019_us_nation_5m.shp",
-                    newfilename,
-                    filename,
-                ],
-                check=True,
-            )
+            newfilename = filename.replace("/all/", "/state-leg/house/")
+            filenames.append(newfilename)
+            if os.path.exists(newfilename):
+                print(f"{newfilename} exists, skipping")
+            else:
+                print(f"{filename} => {newfilename}")
+                subprocess.run(
+                    [
+                        "ogr2ogr",
+                        "-clipsrc",
+                        "data/geospatial/source/cb_2019_us_nation_5m.shp",
+                        newfilename,
+                        filename,
+                    ],
+                    check=True,
+                )
     # for senate files
-    for filename in sorted(glob.glob("data/geospatial/all/*Senate.geojson")): 
-        newfilename = filename.replace("/all/", "/state-leg/senate/")
-        filenames.append(newfilename)
-        if os.path.exists(newfilename):
-            print(f"{newfilename} exists, skipping")
-        else:
-            print(f"{filename} => {newfilename}")
-            subprocess.run(
-                [
-                    "ogr2ogr",
-                    "-clipsrc",
-                    "data/geospatial/source/cb_2019_us_nation_5m.shp",
-                    newfilename,
-                    filename,
-                ],
-                check=True,
-            )
+    for filename in sorted(glob.glob("data/geospatial/all/*Senate.geojson")):
+        if filename in ['data/geospatial/all/DE-Senate.geojson', 'data/geospatial/all/FL-Senate.geojson', 'data/geospatial/all/LA-Senate.geojson',
+        'data/geospatial/all/MD-Senate.geojson', 'data/geospatial/all/NC-Senate.geojson', 'data/geospatial/all/NJ-Senate.geojson', 'data/geospatial/all/OH-Senate.geojson',
+        'data/geospatial/all/PR-Senate.geojson']:
+            print('{filename} is a bad file. Passing.')
+        else: 
+            newfilename = filename.replace("/all/", "/state-leg/senate/")
+            filenames.append(newfilename)
+            if os.path.exists(newfilename):
+                print(f"{newfilename} exists, skipping")
+            else:
+                print(f"{filename} => {newfilename}")
+                subprocess.run(
+                    [
+                        "ogr2ogr",
+                        "-clipsrc",
+                        "data/geospatial/source/cb_2019_us_nation_5m.shp",
+                        newfilename,
+                        filename,
+                    ],
+                    check=True,
+                )
 
     print("Reproject geojsons")
     filenames = []
