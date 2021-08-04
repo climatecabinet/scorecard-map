d6274ad2141216fa4efffa7d0ab00e84a1292251
shelbygreen <greenshelby@gmail.com>
2021-08-04 09:51:56 -0400

    changed from experimental to production db

--- map/gatsby-config.js
+++ map/gatsby-config.js
@@ -13,6 +13,9 @@ module.exports = {
     author: config.schema.author,
     mapboxToken: `pk.eyJ1IjoicGFzaWgiLCJhIjoiY2pybzJqdTVjMHJzeDQ0bW80aGdzaXV3ayJ9.yxD8Nqu7FLnf8-lBo7F1zQ`
   },
+  flags: {
+    FAST_DEV: true,
+  },
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
@@ -25,9 +28,9 @@ module.exports = {
         // create Apollo Link manually. can return a Promise.
         createLink: () => 
           createHttpLink({
-            uri: 'https://us-west-2.aws.realm.mongodb.com/api/client/v2.0/app/ccscorecard-experimental-gubqp/graphql',
+            uri: 'https://us-west-2.aws.realm.mongodb.com/api/client/v2.0/app/climate-cabinet-production-esyps/graphql',
               headers: {
-                apiKey: `A1RJYVgCktjnwsajZuqOPrqHE14NANh0I0lOIde5Fbxdz1r80jItkfe4QiZz6gdV`
+                apiKey: `qsyikbBilDOKsU3WbyyrFXaKWjf2crgqoKqacrz47UeZl7GOEj8PiQ7d7nxXvRny`
               },
               fetch,
           })
