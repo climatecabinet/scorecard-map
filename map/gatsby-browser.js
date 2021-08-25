59d0e2fb93ede3edf6543a3a4010c2e9b0dcc1f2
Michael Chang <mcchang@users.noreply.github.com>
2021-08-25 10:41:42 +0700

    Add doc for wrapRootElement

--- map/gatsby-browser.js
+++ map/gatsby-browser.js
@@ -1,1 +1,3 @@
-export { wrapRootElement } from './src/data/apollo/wrap_root_element';
+// Wrap the root element with ApolloProvider
+// https://www.gatsbyjs.com/docs/reference/config-files/gatsby-browser/#wrapRootElement
+export { wrapRootElementWithApolloProvider as wrapRootElement } from './src/data/apollo/wrap_root_element';
--- map/src/data/apollo/wrap_root_element.js
+++ map/src/data/apollo/wrap_root_element.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { ApolloProvider } from '@apollo/client';
 import { client } from './client';
 
-export const wrapRootElement = ({ element }) => (
+/**
+ * Wrap an element with ApolloProvider
+ */
+export const wrapRootElementWithApolloProvider = ({ element }) => (
   <ApolloProvider client={client}>{element}</ApolloProvider>
 );
